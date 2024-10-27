2e7c1ec0d682271284a15188b825fd78004ba51a
Berkkan Rençber <berkkanrencber@gmail.com>
2024-10-27 20:39:14 +0300

    Added annotations for models

--- api/src/main/java/com/pokemonreview/api/models/Pokemon.java
+++ api/src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -1,9 +1,21 @@
 package com.pokemonreview.api.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Pokemon {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // unique id creation
     private int id;
     private String name;
     private String type;
